bb91209bdea2ee0bf5d8bacea400df1ffd18527d
Wagner Xavier <37937945+wagnerxavierp@users.noreply.github.com>
2020-11-05 17:27:17 -0300

    Create 1016 - Distância.cpp

---
+++ C++/1016 - Distância.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define fastIO ios_base::sync_with_stdio(0);
+
+using namespace std;
+
+
+int main(){
+    fastIO;
+
+    int quilometros;
+
+    cin >> quilometros;
+
+    cout << quilometros * 2 << " minutos\n";
+
+    return 0;
+
+}
